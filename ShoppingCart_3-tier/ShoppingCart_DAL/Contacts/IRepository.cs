27b2301d48e612ed3d00d66579601cc0fd7bdb96
DatNguyen1091 <nvdat0204@gmail.com>
2023-05-11 15:30:19 +0700

    DAL: add interface IRepository

---
+++ ShoppingCart_3-tier/ShoppingCart_DAL/Contacts/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using ShoppingCart_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ShoppingCart_DAL.Data;
+
+
+namespace ShoppingCart_DAL.Contacts
+{
+    public interface IRepository<T>
+    {
+        public List<T> GetAll(int? page);
+        public T GetById(int id);
+        public T Create(T model);
+        public T Update(int id, T model);
+        public string Delete(int id);
+    }
+}
