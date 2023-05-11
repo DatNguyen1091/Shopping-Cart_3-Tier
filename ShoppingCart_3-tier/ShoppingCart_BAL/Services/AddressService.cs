60527a8084d37f8ed8fdc51c6921e1735207a459
DatNguyen1091 <nvdat0204@gmail.com>
2023-05-11 15:35:39 +0700

    BAL: + AddressService

---
+++ ShoppingCart_3-tier/ShoppingCart_BAL/Services/AddressService.cs
@@ -0,0 +1,44 @@
+﻿using ShoppingCart_DAL.Models;
+using ShoppingCart_DAL.Repositories;
+using Microsoft.Extensions.Options;
+using ShoppingCart_DAL.Data;
+
+namespace ShoppingCart_BAL.Services
+{
+    public class AddressesService 
+    {
+        private AddressesRepository _addressesRepository;
+
+        public AddressesService(IOptions<Connection> connection)
+        {
+            _addressesRepository = new AddressesRepository(connection);
+        }
+
+        public List<Addresses> GetAllAddresses(int? page)
+        {
+            return _addressesRepository.GetAll(page);
+        }
+
+        public Addresses GetAddressById(int id)
+        {
+            return _addressesRepository.GetById(id);
+        }
+
+        public Addresses CreateAddress(Addresses model)
+        {
+            return _addressesRepository.Create(model);
+        }
+
+        public Addresses UpdateAddress(int id, Addresses model)
+        {
+            var result = _addressesRepository.Update(id, model);
+            return result;
+        }
+
+        public string RemoveAddress(int id)
+        {
+            var result = _addressesRepository.Delete(id);
+            return result;
+        }
+    }
+}
