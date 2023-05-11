7036b2f44a75ddfcd2ad7ed0bd3284be5dd044b1
DatNguyen1091 <nvdat0204@gmail.com>
2023-05-11 15:36:55 +0700

    + API AddressesController

---
+++ ShoppingCart_3-tier/ShoppingCart/Controllers/AddressesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart_DAL.Models;
+using ShoppingCart_BAL.Services;
+using Microsoft.Extensions.Options;
+using ShoppingCart_DAL.Data;
+
+namespace ShoppingCart.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddressesController : ControllerBase
+    {
+        private readonly AddressesService _addressesService;
+
+        public AddressesController(IOptions<Connection> connection)
+        {
+            _addressesService = new AddressesService(connection);
+        }
+
+        [HttpGet]
+        public List<Addresses> GetAddresses(int? page)
+        {
+            return _addressesService.GetAllAddresses(page);
+        }
+
+        [HttpGet("{id}")]
+        public Addresses GetAddressId(int id)
+        {
+            return _addressesService.GetAddressById(id);
+        }
+
+        [HttpPost]
+        public Addresses PostAddress(Addresses model)
+        {
+            return _addressesService.CreateAddress(model);
+        }
+
+        [HttpPut("{id}")]
+        public Addresses PutAddress(int id, Addresses model)
+        {
+            return _addressesService.UpdateAddress(id, model);
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteAddress(int id)
+        {
+            return _addressesService.RemoveAddress(id);
+        }
+    }
+}
