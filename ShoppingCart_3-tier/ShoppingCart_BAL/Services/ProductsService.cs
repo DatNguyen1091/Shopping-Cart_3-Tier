cb4756abf6c6629b5401755edc322c0debefbe39
DatNguyen1091 <nvdat0204@gmail.com>
2023-05-18 22:06:57 +0700

    Add ProductsService

---
+++ ShoppingCart_3-tier/ShoppingCart_BAL/Services/ProductsService.cs
@@ -0,0 +1,41 @@
+﻿using ShoppingCart_DAL.Models;
+using ShoppingCart_DAL.Repositories;
+
+namespace ShoppingCart_BAL.Services
+{
+    public class ProductsService
+    {
+        private ProductsRepository _productsRepository;
+        public ProductsService(ProductsRepository productsRepository)
+        {
+            _productsRepository = productsRepository;
+        }
+
+        public List<Products> GetAllProducts(int? page)
+        {
+            return _productsRepository.GetAll(page);
+        }
+
+        public Products GetProductsById(int id)
+        {
+            return _productsRepository.GetById(id);
+        }
+
+        public Products CreateProducts(Products model)
+        {
+            return _productsRepository.Create(model);
+        }
+
+        public Products UpdateProducts(int id, Products model)
+        {
+            var result = _productsRepository.Update(id, model);
+            return result;
+        }
+
+        public string RemoveProducts(int id)
+        {
+            var result = _productsRepository.Delete(id); 
+            return result;
+        }
+    }
+}
