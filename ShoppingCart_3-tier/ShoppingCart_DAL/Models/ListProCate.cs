633a07cd36df6b46a08cda4a91a059dd6d75298a
DatNguyen1091 <nvdat0204@gmail.com>
2023-06-20 14:48:52 +0700

    Add model ListProCate

---
+++ ShoppingCart_3-tier/ShoppingCart_DAL/Models/ListProCate.cs
@@ -0,0 +1,10 @@
+﻿using ShoppingCart_DAL.Data;
+
+namespace ShoppingCart_DAL.Models
+{
+    public class ListProCate
+    {
+        public Products? products { get; set; }
+        public Categories? categories { get; set; }
+    }
+}
