d58b699c71c161ca6112b7d109614ad623070d42
DatNguyen1091 <nvdat0204@gmail.com>
2023-07-15 10:41:10 +0700

    EnableCors

--- ShoppingCart_3-tier/ShoppingCart/Controllers/ProductsController.cs
+++ ShoppingCart_3-tier/ShoppingCart/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using ShoppingCart_BAL.Services;
 using ShoppingCart_DAL.Models;
 
@@ -6,6 +7,7 @@ namespace ShoppingCart.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("AllowOrigin")]
     public class ProductsController : ControllerBase
     {
         private readonly ProductsService _productsService;
